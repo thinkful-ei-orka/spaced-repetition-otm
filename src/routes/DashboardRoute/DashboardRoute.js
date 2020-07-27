27dc0ca424e10d529293fb78ada026e77d3e48e9
Taeil Kwak <taeil2@gmail.com>
2020-07-27 14:17:46 -0500

    set up the dashboard page

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,11 +1,81 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import config from '../../config';
+import TokenService from '../../services/token-service';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faCheckCircle, faTimes, faTimesCircle  } from '@fortawesome/free-solid-svg-icons';
 
 class DashboardRoute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: null,
+      words: null
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(json => {
+        this.setState({
+          language: json.language,
+          words: json.words
+        })
+      })
+      .catch(e => console.log(e));
+  }
+
   render() {
+    let languageName = '';
+    let totalScore = 0;
+    let words = [];
+
+    if (this.state.language) {
+      languageName = this.state.language.name;
+      totalScore = this.state.language.total_score;
+    }
+
+    if (this.state.words) {
+      this.state.words.forEach((word, i) => {
+        words.push(
+          <div key={i}>
+            <div>{word.original}</div>
+            <div>{word.correct_count}</div>
+            <div>{word.incorrect_count}</div>
+          </div>
+        )
+      })
+    }
+
     return (
-      <section>
-        implement and style me
-      </section>
+      <>
+        <section>
+          <h2>{languageName}</h2>
+          <div>Total Score: {totalScore}</div>
+          <Link to="/learn">Start Practicing</Link>
+        </section>
+        <section>
+          <div>
+            <div>Words to practice</div>
+            {/* Choose the plain or circle icon */}
+            <div><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckCircle} /></div>
+            <div><FontAwesomeIcon icon={faTimes} /><FontAwesomeIcon icon={faTimesCircle} /></div>
+          </div>
+          <div>
+            {words}
+          </div>
+        </section>
+      </>
     );
   }
 }
--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,11 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import config from '../../config';
+import TokenService from '../../services/token-service';
+
 
 class LearningRoute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: ''
+    }
+  }
+
+  handleChange = (value) => {
+    this.setState({word: value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let guess = {
+      word: this.state.word
+    }
+
+    fetch(`${config.API_ENDPOINT}/api/language/guess`, {
+      method: 'POST',
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+
+      },
+      body: JSON.stringify(guess),
+    })
+      .then(res => console.log(res))
+      .catch(e => console.log(e))
+  }
+
+  componentDidMount() {
+    fetch(`${config.API_ENDPOINT}/api/language/head`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res => console.log(res))
+      .catch(e => console.log(e));
+  }
+
   render() {
     return (
-      <section>
-        implement and style me
-      </section>
+      <>
+        <section>
+          <h2>Translate the word:</h2>
+          <div>Bonjour</div>
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <label className="Label" htmlFor='guess'>What's the translation for this word?</label>
+            <input type="text" id="guess" value={this.state.word} onChange={e => this.handleChange(e.target.value)}></input>
+            <button type="submit">Submit your answer</button>
+          </form>
+        </section>
+        <section>
+          <h3>Your total score is: 5000</h3>
+          <p>
+            You have answered this word correctly 123 times.
+            You have answered this word incorrectly 123 times.
+          </p>
+        </section>
+      </>
     );
   }
 }
