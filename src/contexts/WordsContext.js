62e1296c98357d468bcc1bf761bf1db3d5c33095
Taeil Kwak <taeil2@gmail.com>
2020-07-28 14:22:09 -0500

    started adding words context

---
+++ src/contexts/WordsContext.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+const WordsContext = React.createContext({
+  language: null,
+  words: null,
+  setContext: () => {},
+})
+
+export default WordsContext
+
+export class WordsProvider extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      language: null,
+      words: null
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  setContext = (newContext) => {
+    this.setState({newContext})
+  }
+
+  render() {
+    const value = {
+      language: this.state.language,
+      words: this.state.words,
+      setContext: this.setContext,
+    }
+    return (
+      <WordsContext.Provider value={value}>
+        {this.props.children}
+      </WordsContext.Provider>
+    )
+  }
+}
