4ec5f86628d28f4062b8c6eaa55da6ff5e6f305f
Taeil Kwak <taeil2@gmail.com>
2020-08-02 22:53:35 -0500

    added the new api endpoint to config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'https://whispering-taiga-99292.herokuapp.com/api',
+  API_ENDPOINT: 'https://spaced-repetition-api-otm.herokuapp.com/api',
   TOKEN_KEY: 'spaced-repetition-jwt-secret',
 }
